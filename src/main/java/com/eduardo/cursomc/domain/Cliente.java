dba2d5c3b1d80a9901e23b62322ce450158e6d20
Eduardo Padilha <epadilha@gmail.com>
2019-05-30 21:13:39 -0300

    Endpoint/clientes/{id} disponivel

--- src/main/java/com/eduardo/cursomc/domain/Cliente.java
+++ src/main/java/com/eduardo/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.eduardo.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity 
 public class Cliente implements Serializable {
@@ -28,8 +29,11 @@ public class Cliente implements Serializable {
 	private String cpfOucnpj;
 	private Integer tipo;
 	
+	
+	@JsonManagedReference
 	@OneToMany(mappedBy="cliente") 
 	private List<Endereco> enderecos = new ArrayList<>();
+	
 	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
