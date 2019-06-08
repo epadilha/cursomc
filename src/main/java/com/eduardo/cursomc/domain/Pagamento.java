dde9a3d277938c6fb5c00ae7509cb903fa4bacbe
Eduardo Padilha <epadilha@gmail.com>
2019-06-08 13:36:46 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/eduardo/cursomc/domain/Pagamento.java
+++ src/main/java/com/eduardo/cursomc/domain/Pagamento.java
@@ -9,7 +9,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.eduardo.cursomc.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -19,7 +19,7 @@ public class Pagamento {
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
